b822874eaaf360dad66053f9a0d88a06b0ecc84a
Bradly Sharpe <bradly@bradlysharpe.com.au>
2021-09-18 22:09:15 +1000

    testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,31 @@
-const path = require("path")
+const path = require('path')
+const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/index.js"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js",
-    library: "$",
-    libraryTarget: "umd",
+  entry: {
+    server: './src/index.js',
   },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].js'
+  },
+  target: 'node',
+  node: {
+    // Need this when working with express, otherwise the build fails
+    __dirname: false,   // if you don't put this is, __dirname
+    __filename: false,  // and __filename return blank or /
+  },
+  externals: [nodeExternals()], // Need this to avoid error when working with Express
   module: {
     rules: [
       {
-        test: /\.(js)$/,
+        test: /\.js$/,
         exclude: /node_modules/,
-        use: "babel-loader",
-      },
-    ],
+        loader: "babel-loader",
+      }
+    ]
   },
-  mode: "development",
+  plugins: []
 }
